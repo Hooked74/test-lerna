6dc4280ed47f035760295610a115d0fcebca809e
Новиков Игорь <novikovio74@gmail.com>
2021-09-14 10:28:06 +0500

    chain from packages

--- packages/pack1/lib/pack1.js
+++ packages/pack1/lib/pack1.js
@@ -2,6 +2,7 @@
 
 const consola = require("consola");
 const chalk = require("chalk");
+const pack3 = require("test-lerna-pack3");
 
 module.exports = pack1;
 
@@ -9,5 +10,6 @@ function pack1() {
   return "render-pack1";
 }
 
+console.log(pack3());
 console.log(chalk.cyan("ololo"));
 consola.success(chalk.blue("Built!"));
