f7e6099f8adbeca5ea0de313e42ba93a513ddc8c
Новиков Игорь <novikovio74@gmail.com>
2022-06-16 15:55:08 +0500

    123

--- packages/pack4/src/main.tsx
+++ packages/pack4/src/main.tsx
@@ -1,16 +1,16 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import './index.css'
-// import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// )
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
-import pack3 from "test-lerna-pack3";
-import pack5 from "test-lerna-pack5";
+// import pack3 from "test-lerna-pack3";
+// import pack5 from "test-lerna-pack5";
 
-console.log(pack3());
+// console.log(pack3());
